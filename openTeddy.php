c272707b940c14d68a1c9afac9eeb277536338c4
edward <thumbd12856@gmail.com>
2016-08-07 16:02:40 +0800

    version 3: Pinocchio, without bridge and unity build

--- openTeddy.php
+++ openTeddy.php
@@ -1,28 +1,26 @@
     <?php
-    
-    /*
-	$com = '"E:\Programs\Editor\Unity.exe" -batchmode -quit -projectPath "G:\Respository\Unity Projects\autogen" -user yanagiragi -buildWindows64Player "C:\Users\Ragi\Desktop\1.exe"';
-    */
+
+    //temp
 	$dest = '"./builds/build.exe"';
     $com = '"C:\Program Files\Unity\Editor\Unity.exe" -batchmode -quit -projectPath "C:\xampp\htdocs\TeddyWebUI\autogen" -user thumbd12856 -buildWindows64Player "C:\xampp\htdocs\TeddyWebUI\builds\build.exe"';
     $mv = 'move C:\Users\USER\Downloads\mymodel.fbx "C:\xampp\htdocs\TeddyWebUI\autogen\Assets\Resources\Models\mymodel.fbx"';
 
 
-    if($_GET['option'] == 'teddy'){
+    if($_GET['option'] == 'drawTeddy'){
         exec('java -jar teddy\teddy.jar', $value);
-        echo "exec java!";
+        var_dump($value);
     }
     
     else if($_GET['option'] == 'renameObj') {
+        //temp
         exec('move C:\xampp\htdocs\TeddyWebUI\test.obj "C:\xampp\htdocs\TeddyWebUI\models\mymodel.obj"', $value);
         var_dump($value);
     }
 
-    else if($_GET['option'] == 'fbxConverter'){
-        exec('"C:\xampp\htdocs\TeddyWebUI\FbxConverter.exe" "C:\xampp\htdocs\TeddyWebUI\model\human.obj" "C:\xampp\htdocs\TeddyWebUI\model\human.fbx"', $value);
-        return $value;
+    else if($_GET['option'] == 'bridge') {
+        // ragi: bridge
     }
-    
+
     else if($_GET['option'] == 'unity'){
         echo "Drink volka!";
         exec($mv, $returnVal);        
--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    exec('"C:\xampp\htdocs\TeddyWebUI\FbxConverter.exe" "C:\xampp\htdocs\TeddyWebUI\model\human.obj" "D:\human.fbx"', $value);
-    return $value;
-?>
